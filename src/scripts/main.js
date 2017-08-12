2a7dccf561b534c88226a87cc45093f1f2d71b69
wojtek534 <wojtek.rzepkaa@gmail.com>
2017-08-12 13:23:18 +0200

    0.8

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,9 +7,41 @@ $('a[href*="#"]').on('click', function (e) {
     }, 500, 'linear');
 });
 // Reveal
-window.sr = ScrollReveal({reset: true});
+window.sr = ScrollReveal({
+    reset: true
+});
 
 sr.reveal('table');
 sr.reveal('#library');
 sr.reveal('#about');
 sr.reveal('.project-item');
+
+// Moving bars
+var runbar = function (bar, stop) {
+    $('.progress-box .progress .' + bar).animate({
+            width: stop
+        },
+        4000);
+
+}
+var displayed = false;
+$(window).scroll(function () {
+    if (displayed == false) {
+        var scroll = $(window).scrollTop();
+        if (scroll > 100) {
+            // Programming
+            runbar('bar-c', '40%');
+            runbar('bar-html', '60%');
+            runbar('bar-js', '35%');
+            // Data
+            runbar('bar-r', '25%');
+            runbar('bar-sql', '50%');
+            runbar('bar-excel', '60%');
+            // Rest
+            runbar('bar-ms', '60%');
+            runbar('bar-pho', '20%');
+            runbar('bar-vsc', '60%');
+            displayed = true;
+        }
+    }
+});
